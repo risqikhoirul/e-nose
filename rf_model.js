2dc4a24bfc71051c8a4e4ca4d2f8e4d1d175606f
M Khoirul Risqi <82764602+risqikhoirul@users.noreply.github.com>
2025-10-27 12:49:11 +0700

    Delete rf_model.js

--- rf_model.js
+++
@@ -1,6 +0,0 @@
-// rf_model.js (auto-generated)
-const rf_model = {
-  classes: ["Makanan Basi", "Makanan Segar"],
-  weights: [0.37256322271343223, 0.4535143380248273, 0.1739224392617404],
-  threshold: 3500
-};
