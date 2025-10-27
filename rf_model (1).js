72011dbe449b0fd80028d7a9184544f84df012ea
M Khoirul Risqi <82764602+risqikhoirul@users.noreply.github.com>
2025-10-27 12:36:55 +0700

    Add files via upload

---
+++ rf_model (1).js
@@ -0,0 +1,6 @@
+// rf_model.js (auto-generated)
+const rf_model = {
+  classes: ["Makanan Basi", "Makanan Segar"],
+  weights: [0.37256322271343223, 0.4535143380248273, 0.1739224392617404],
+  threshold: 3500
+};
